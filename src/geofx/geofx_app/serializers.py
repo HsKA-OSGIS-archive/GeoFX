5e18d1dfb45aad85850c4f947511344dfd496426
Marco Lechner, GeoBoink <marco@marcolechner.de>
2021-01-31 14:34:50 +0100

    changes to respect PEP-8 (https://www.python.org/dev/peps/pep-0008/)

--- src/geofx/geofx_app/serializers.py
+++ src/geofx/geofx_app/serializers.py
@@ -1,11 +1,10 @@
 from rest_framework import serializers
-
 from .models import Map
+
 
 class MapSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Map
-        fields = ('url_name', 'display_name', 'description',\
-            'title', 'fence_enter_message', 'fence_leave_message', 'map_center',\
-            'map_zoom_level', 'basemap', 'geofencing_layer_name')
-        
+        fields = ('url_name', 'display_name', 'description',
+                  'title', 'fence_enter_message', 'fence_leave_message', 'map_center',
+                  'map_zoom_level', 'basemap', 'geofencing_layer_name')
